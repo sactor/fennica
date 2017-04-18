e7b5ebc5705cccb78d09fedc9ba87b4370017dcd
Antti Kumpulainen <antti.kumpulainen@crasman.fi>
2017-04-18 20:46:48 +0300

    Initial version

--- README.md
+++ README.md
@@ -1,2 +1,81 @@
 # fennica
 Custom API for The National Bibliography of Finland, Fennica
+
+This is an unofficial API which parses HTML results and will break if the HTTP interface of Fennica is altered.
+
+## Install
+```bash
+npm install fennica
+```
+
+```javascript
+const fennica = require('fennica');
+const SEARCH_MODE = require('fennica').SEARCH_MODE;
+```
+
+## API
+
+### `fennica.search(search, mode)`
+
+Executes a search.
+
+#### Parameters:
+
+ * `search`, a String, search string
+ * `mode`, a String, one of the modes available in SEARCH_MODE constant.
+
+#### Returns:
+
+Returns a Promise which will resolve once the search is complete. Promise result is an object with the following properties:
+
+```javascript
+{
+  "results": [ // Results array
+    {
+      "author": { // Author information
+        "lastname": "Pratchett", // Last name of the author, If author has only one name, e.g. Homeros, it will be here
+        "firstname": "Terry", // Optional
+        "additional": [ // Optional, describes author (in Finnish)
+          "kirjoittaja"
+        ] 
+      },
+      "original_title": "Witches abroad", // Title in original language
+      "title": "Noitia maisemissa", // Localised title
+      "language": "suomi", // Language
+      "publishing_information": { // Publishing information
+        "place": "Hämeenlinna", // Place of publishing
+        "publisher": "Karisto", // Publisher
+        "year": "2000" // Year of publishing
+      },
+      "measurements": { // Page measurements
+        "pages": "309", // Page count
+        "height": "18" // Height, in cm
+      },
+      "additional": { // Optional, Additional information about the book
+        "editions": [ // Parsed edition information from the additional information
+          {
+            "edition": "3", // Edition number
+            "year": "2002" // Edition year
+          }
+        ],
+        "raw": "Lisäpainokset: 3. p. 2002." // Raw additional information from the bibliography
+      },
+      "original_language": "eng", // Optional, Original language
+      "isbn": [ // ISBN information, may contain multiple ISBNs if the book has several editions
+        "951-23-4113-1 nidottu"
+      ],
+      "udk_class": "820 -3", // The UDC class (or UDK in Finnish)
+      "coauthors": [ // Co authors of the book
+        {
+          "lastname": "Sinkkonen",
+          "firstname": "Marja", // Optional
+          "additional": [ // Optional, describes co-author (in Finnish)
+            "kääntäjä"
+          ] 
+        }
+      ]
+    }
+  ],
+  "url": "https://fennica.linneanet.fi/vwebv/search?searchArg=Noitia%20maisemissa&searchCode=TALL&setLimit=2&recCount=10&searchType=1&page.search.search.button=Hae" // URL used for this search
+}
+```
---
+++ index.ts
@@ -0,0 +1,371 @@
+import * as jsdom from 'jsdom';
+import * as _debug from 'debug';
+
+let debug = _debug('fennica');
+
+interface Window {
+  $: any;
+}
+
+namespace Fennica {
+  export type Author = {
+    lastname: string,
+    firstname?: string,
+    additional?: string[]
+  }
+  export type Edition = {
+    edition: number,
+    year: number
+  }
+  export type Additional = {
+    editions: Edition[],
+    raw: string
+  }
+  export type Measurements = {
+    pages?: number,
+    height?: number,
+    additional?: string
+  }
+  export type PublishingInformation = {
+    place?: string,
+    publisher?: string,
+    year?: number
+  }
+  export type Result = {
+    author: Author,
+    original_title: string,
+    title: string,
+    language: string,
+    publishing_information?: PublishingInformation,
+    measurements?: Measurements,
+    additional?: Additional,
+    original_language?: string,
+    isbn: string[],
+    udk_class: string,
+    coauthors: Author[]
+  }
+  export type SearchResult = {
+    results: Result[],
+    url: string
+  }
+
+  export enum SEARCH_MODE {
+    ISBN,
+    TITLE,
+    AUTHOR
+  }
+
+  const SEARCH_MODE_MAP = {
+    [SEARCH_MODE.ISBN]: '020B',
+    [SEARCH_MODE.TITLE]: 'TALL',
+    [SEARCH_MODE.AUTHOR]: 'NAME'
+  };
+
+  const fields:{[key:string]: string} = {
+    'Tekijä:': 'author',
+    'Teos:': 'original_title',
+    'Nimeke:': 'title',
+    'Kieli:': 'language',
+    'Julkaistu:': 'publishing_information',
+    'Alkuteoksen kieli:': 'original_language',
+    'UDK-luokitus:': 'udk_class',
+    'ISBN:': 'isbn',
+    'Ulkoasu:': 'measurements',
+    'Asiasana:': 'keywords',
+    'Huomautus:': 'additional',
+    'Muu(t) tekijä(t):': 'coauthors'
+  };
+
+  const specials:{[key: string]: Function} = {
+    author: (input: string, $: any): Author | false => {
+      let authorinfo = $('<div>' + input + '</div>').text().split('\n')[0].split(',');
+      return handleSingleAuthorRow(authorinfo);
+    },
+    coauthors: (input: string, $: any): Author[] => {
+      let rows = $('<div>' + input + '</div>').text().split('\n');
+      let authors: Author[] = [];
+      rows.forEach((element: string) => {
+        let raw = element.trim();
+        if (raw.length) {
+          let author = handleSingleAuthorRow(raw.split(','));
+          if (author !== false) {
+            authors.push(author);
+          }
+        }
+      });
+      return authors;
+    },
+    additional: (input: string, $: any): Additional => {
+      let raw = $('<div>' + input + '</div>').text();
+      let re = /(\d{1,2})\.(?:-(\d{1,2})\.)? p\. (\d{4})\./g;
+      let result;
+      let additional: Additional = {
+        editions: [],
+        raw
+      };
+      while ((result = re.exec(raw)) !== null) {
+        let editionStart = parseInt(result[1]);
+        let editionEnd = parseInt(result[2]);
+        let year = parseInt(result[3]);
+        if (editionEnd) {
+          for (let i: number = editionStart; i <= editionEnd; i++) {
+            additional.editions.push({ edition: i, year });
+          }
+        } else {
+          additional.editions.push({ edition: editionStart, year });
+        }
+      }
+      return additional;
+    },
+    title: (input: string): string => {
+      return input.split('/')[0].trim();
+    },
+    isbn: (input: string): string[] => {
+      return input.split('<br>').map((val) => {
+        return val.trim();
+      }).filter((val) => {
+        return val.length > 0;
+      });
+    },
+    original_title: (input: string, $: any): string => {
+      return $('<div>' + input + '</div>').text().replace(/[\[\]]/g, '').trim();
+    },
+    measurements: (input: string, $: any): Measurements => {
+      let measurements: Measurements = {};
+      let parts: string[] = $('<div>' + input + '</div>').text().split(/[;:,]/);
+      parts.map((val) => {
+        let propparts = val.trim().split(' ');
+        if (propparts.length > 1) {
+          switch (propparts[1]) {
+            case 'cm':
+              measurements['height'] = parseInt(propparts[0]);
+              break;
+            case 'sivua':
+            case 's.':
+              measurements['pages'] = parseInt(propparts[0]);
+              break;
+            default:
+              debug('Dunno what is this prop:');
+              debug(val);
+          }
+        } else {
+          switch (propparts[0]) {
+            case 'kuvitettu':
+              measurements['additional'] = propparts[0];
+              break;
+            default:
+              debug('Dunno how to handle parts for measurements prop: ');
+              debug(val);
+          }
+        }
+      });
+      return measurements;
+    },
+    keywords: (input: string, $: any): string[] => {
+      let parts: string[] = $('<div>' + input + '</div>').text().split('\n');
+      parts = parts.map((part) => {
+        return part.replace('(ysa)', '').trim();
+      });
+      parts = parts.filter((part) => {
+        return part.length;
+      });
+      return parts;
+    },
+    publishing_information: (input: string, $: any): PublishingInformation => {
+      let parts: string[] = $('<div>' + input + '</div>').text().split(',');
+      let dateinfo: PublishingInformation = {};
+      parts.map((part) => {
+        let partinfo = part.trim().replace(/[\[\].]/g, '');
+        if (isNaN(parseInt(partinfo))) {
+          let subparts = partinfo.split(':');
+          dateinfo['place'] = subparts[0].trim();
+          if (subparts.length > 1) {
+            dateinfo['publisher'] = subparts[1].trim();
+          }
+          if (subparts.length > 2) {
+            debug('PARSE NOTICE: Apparently possible to have more parts');
+            debug(subparts);
+          }
+        } else {
+          dateinfo['year'] = parseInt(partinfo);
+        }
+      });
+      return dateinfo;
+    }
+  };
+  function handleSingleAuthorRow(authorinfo: string[]): Author | false {
+    let author: Author = { lastname: '' };
+    for (let i = 0; i < authorinfo.length; i++) {
+      let info = authorinfo[i].trim();
+      switch (i) {
+        case 0:
+          author['lastname'] = info.replace(/\.$/, '');
+          break;
+        case 1:
+          author['firstname'] = info.replace(/([^A-Z])\.$/, '$1');
+          break;
+        default:
+          info = info.replace(/\.$/, '');
+          if (info.includes('ennakkotieto')) {
+            return false;
+          }
+          if (typeof author['additional'] === 'undefined') {
+            author['additional'] = [];
+          }
+          author['additional'].push(info);
+      }
+    }
+    return author;
+  }
+
+  let handleField = (row: any, $: any): any => {
+    if (row.find('.holdingsLabel').length > 0) {
+      return null;
+    }
+    let field: string = row.find('.fieldLabelSpan').text();
+    if (typeof fields[field] !== 'undefined') {
+      let value;
+      if (typeof specials[fields[field]] !== 'undefined') {
+        value = row.find('.subfieldData').html().trim();
+        value = specials[fields[field]](value, $);
+        if (value === false) {
+          return {
+            field: 'rejected',
+            value: null
+          };
+        }
+      } else {
+        value = row.find('.subfieldData').text().trim();
+      }
+      return {
+        field: fields[field],
+        value: value
+      };
+    } else {
+      return null;
+    }
+  };
+
+  function handleSingleBook(search: string, $: any): any | Result {
+    let bibTags = $('.bibTag');
+    let bookObject: {[key: string]: any} = {};
+    let rejected = false;
+    bibTags.each((i: number, ele: any) => {
+      let field = handleField($(ele), $);
+      if (field !== null) {
+        if (field.field === 'rejected') {
+          // Rejected result
+          rejected = true;
+          return false;
+        }
+        bookObject[field.field] = field.value;
+      }
+    });
+    if (rejected) {
+      return null;
+    }
+    if (typeof bookObject['original_title'] !== 'undefined') {
+      return bookObject;
+    }
+    if (typeof bookObject['title'] !== 'undefined') {
+      bookObject['original_title'] = bookObject['title'];
+      return bookObject;
+    }
+    debug('For information, this book did not have title and was skipped from results:');
+    debug(bookObject);
+    return null;
+  }
+
+  function handleSearchResult(search: string, mode: SEARCH_MODE, $: any): Promise<Result[]> {
+    return new Promise((resolve, reject) => {
+      if ($('.noHitsError').length) {
+        resolve([]);
+      }
+      switch (mode) {
+        case SEARCH_MODE.ISBN:
+          resolve(handleSingleBook(search, $));
+          break;
+        case SEARCH_MODE.TITLE:
+          let links = $('.line1Link');
+          if (!links.length) {
+            // Probably a direct hit, handle a single book result
+            resolve([handleSingleBook(search, $)]);
+            return;
+          }
+          let promises: Array<Promise<Result>> = [];
+          links = links.each(function () {
+            let titleParts = $(this).text().trim().replace(/\.$/, '').split('/');
+            let title: string;
+            if (titleParts.length > 1) {
+              title = titleParts.slice(0, titleParts.length - 1).join('/').trim();
+            } else {
+              title = titleParts[0];
+            }
+            if (title.toLowerCase().includes(search.toLowerCase())) {
+              // Candidate for a search result, get the book page
+              promises.push(new Promise((linkresolve, linkreject) => {
+                // debug($(this).find('a').attr('href')); // Logs link where single book info is found
+                let href = $(this).find('a').attr('href');
+                debug('starting sub request ' + href);
+                jsdom.env(
+                  'https://fennica.linneanet.fi/vwebv/' + href,
+                  ['http://code.jquery.com/jquery.js'],
+                  (err, window: Window) => {
+                    debug('sub request done ' + href);
+                    if (err) {
+                      debug(err);
+                      linkreject(err);
+                      return;
+                    }
+                    linkresolve(handleSingleBook(search, window.$));
+                  }
+                );
+              }));
+            }
+          });
+          Promise.all(promises).then((results) => {
+            results = results.filter((res) => {
+              return res !== null;
+            });
+            resolve(results);
+          });
+          return;
+      }
+      resolve(null);
+    });
+  }
+
+  export function search(search: string, mode: SEARCH_MODE): Promise<SearchResult> {
+    return new Promise<SearchResult>(
+      (resolve, reject) => {
+        let searchurl = 'https://fennica.linneanet.fi/vwebv/search?searchArg=%s&searchCode=%m&setLimit=2&recCount=10&searchType=1&page.search.search.button=Hae'
+          .replace(/%s/g, encodeURIComponent(search))
+          .replace(/%m/g, SEARCH_MODE_MAP[mode]);
+        debug('starting request ' + searchurl);
+        jsdom.env(
+          searchurl,
+          ['http://code.jquery.com/jquery.js'],
+          (err, window: Window) => {
+            debug('request done ' + searchurl);
+            if (err) {
+              reject(err);
+              return;
+            }
+            handleSearchResult(search, mode, window.$)
+              .then((res) => {
+                resolve({
+                  results: res,
+                  url: searchurl
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+        );
+      }
+    );
+  }
+}
+
+export default Fennica;
